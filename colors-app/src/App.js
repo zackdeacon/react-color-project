63420bd028c912c7fdfb1be15617e015c8d4eef3
Zachary Deacon <zackdeacon347@gmail.com>
2020-12-30 14:40:39 -0800

    created react app added palette

---
+++ colors-app/src/App.js
@@ -0,0 +1,12 @@
+import Pallete from "./Pallete";
+import seedColors from "./seedColors";
+
+function App() {
+  return (
+    <div>
+      <Pallete {...seedColors[4]}/>
+    </div>
+  );
+}
+
+export default App;
