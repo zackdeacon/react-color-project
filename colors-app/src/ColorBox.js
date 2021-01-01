cb65e5fa58850fa3f1773808983d8bfb9f4ee1fb
Zachary Deacon <zackdeacon347@gmail.com>
2020-12-31 16:32:43 -0800

    added overlay css

--- colors-app/src/ColorBox.js
+++ colors-app/src/ColorBox.js
@@ -3,11 +3,30 @@ import "./ColorBox.css"
 import {CopyToClipboard} from "react-copy-to-clipboard";
 
 class ColorBox extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { copied: false };
+        this.changeCopyState = this.changeCopyState.bind(this);
+    }
+    
+    changeCopyState() {
+        console.log(this.state.copied)
+        this.setState({ copied: true }, () => {
+            setTimeout(() => this.setState({ copied: false }), 1500);
+        });
+    }
     render() {
         const {name, background} = this.props; 
+        const {copied} = this.state;
+        console.log(this.state.copied);
         return (
-            <CopyToClipboard text={background}>
+            <CopyToClipboard text={background} onCopy={this.changeCopyState}>
             <div style = {{ background }} className = "ColorBox">
+                <div style={{ background }} className={`copy-overlay ${copied && "show"}`} />
+                <div className= {`copy-message ${copied && "show"}`} >
+                    <h1>Copied!</h1>
+                    <p>{this.props.background}</p>
+                </div>
                 <div className = "copy-container">
                     <div className = "box-content">
                         <span>{ name }</span>
