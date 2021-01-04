1e247815c009d7c8e1d553989dd369386df8a9bf
Zachary Deacon <zackdeacon347@gmail.com>
2021-01-04 13:40:50 -0800

    added slider

--- colors-app/src/App.js
+++ colors-app/src/App.js
@@ -3,10 +3,9 @@ import seedColors from "./seedColors";
 import { generatePalette } from "./ColorHelpers";
 
 function App() {
-  console.log(generatePalette(seedColors[4]))
   return (
     <div>
-      <Palette {...seedColors[4]}/>
+      <Palette palette={generatePalette(seedColors[4])}/>
     </div>
   );
 }
