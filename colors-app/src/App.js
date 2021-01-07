245c0ca312598b58992171f937a5cc43e4cba81a
Zachary Deacon <zackdeacon347@gmail.com>
2021-01-07 13:54:24 -0800

    added the routes for seeded palettes

--- colors-app/src/App.js
+++ colors-app/src/App.js
@@ -5,11 +5,19 @@ import seedColors from "./seedColors";
 import { generatePalette } from "./ColorHelpers";
 
 class App extends Component {
+  findPalette(id) {
+    return seedColors.find(function(palette){
+      return palette.id === id;
+    })
+  }
   render() {
   return (
     <Switch>
     <Route exact path="/" render={() => <h1>PALETTE LIST GOES HERE!</h1>}/>
-    <Route exact path="/palette/:id"/>
+    <Route exact path="/palette/:id" render={routeProps => (
+    <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id)
+      )}/>
+    )}/>
     </Switch>
     // <div>
     //   <Palette palette={generatePalette(seedColors[4])}/>
