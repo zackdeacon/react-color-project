28868084abab986460393f976291312c272745da
Zachary Deacon <zackdeacon347@gmail.com>
2021-01-07 14:06:42 -0800

    added Link components to navigate back to the home page

--- colors-app/src/App.js
+++ colors-app/src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./ColorHelpers";
+import PaletteList from "./PaletteList";
 
 class App extends Component {
   findPalette(id) {
@@ -13,7 +14,7 @@ class App extends Component {
   render() {
   return (
     <Switch>
-    <Route exact path="/" render={() => <h1>PALETTE LIST GOES HERE!</h1>}/>
+    <Route exact path="/" render={() => <PaletteList palettes={seedColors}/>}/>
     <Route 
     exact 
     path="/palette/:id" 
