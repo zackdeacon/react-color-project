08a252624d6e75a348036c79c1cf037b3dee2a3c
Zachary Deacon <zackdeacon347@gmail.com>
2021-01-11 13:44:21 -0800

    rendered actual colors in mini palette

--- colors-app/src/MiniPalette.js
+++ colors-app/src/MiniPalette.js
@@ -4,6 +4,7 @@ import { withStyles } from "@material-ui/styles";
 const styles = {
 root: {
     backgroundColor: "white",
+    border: "1px solid black",
     borderRadius: "5px",
     padding: "0.5rem",
     position: "relative",
@@ -13,8 +14,11 @@ root: {
     }
 }, 
 colors: {
-    backgroundColor: "white"
-
+    backgroundColor: "#dae1e4",
+    height: "150px",
+    width: "100%",
+    borderRadius: "5px",
+    overflow: "hidden"
 },
 title: {
     display: "flex",
@@ -29,16 +33,34 @@ title: {
 emoji: {
     marginLeft: "0.5rem",
     fontSize: "1.5rem"
+},
+miniColor: {
+    height: "25%",
+    width: "20%",
+    display: "inline-block",
+    margin: "0 auto",
+    position: "relative",
+    marginBottom: "-3.5px",
+
 }
 }
 
 function MiniPalette(props) {
-    const { classes, paletteName, emoji } = props;
+    const { classes, paletteName, emoji, colors } = props;
+    const miniColorBoxes = colors.map(color => (
+        <div 
+        className={classes.miniColor}
+        style={{ backgroundColor: color.color }}
+        key={color.name}
+        ></div>
+    ))
     return (
         <div className={classes.root}>
            <div className={classes.colors}>
-            <h5 className={classes.title}>{paletteName}<span className={classes.emoji}>{emoji}</span></h5>
+                {miniColorBoxes}
            </div>
+            <h5 className={classes.title}>{paletteName}<span className={classes.emoji}>{emoji}</span>
+            </h5>
         </div>
     )
 }
