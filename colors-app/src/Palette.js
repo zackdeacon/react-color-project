5805253ee195def3ec3267fffd0f75d13b3d3f12
Zachary Deacon <zackdeacon347@gmail.com>
2021-01-13 14:52:40 -0800

    refactored with JSS

--- colors-app/src/Palette.js
+++ colors-app/src/Palette.js
@@ -27,7 +27,7 @@ class Palette extends Component {
         const { colors, paletteName, emoji, id} = this.props.palette;
         const { level, format } = this.state;
         const colorBoxes = colors[level].map(color => (
-            <ColorBox background={color[format]} name={color.name} key={color.id} id={color.id} paletteId={id} showLink={true}/> 
+            <ColorBox background={color[format]} name={color.name} key={color.id} id={color.id} paletteId={id} showingFullPalette={true}/> 
         ));
         return (
             <div className="Palette">
