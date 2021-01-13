3f95293f61db6f60c5aafec2f37453b6442aaa20
Zachary Deacon <zackdeacon347@gmail.com>
2021-01-13 13:43:58 -0800

    added go back div in single color palette

--- colors-app/src/SingleColorPalette.js
+++ colors-app/src/SingleColorPalette.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import ColorBox from "./ColorBox"
 import Navbar from "./Navbar"
 import PaletteFooter from "./PaletteFooter"
+import { Link } from "react-router-dom";
 
 class SingleColorPalette extends Component {
     constructor(props){
@@ -25,15 +26,18 @@ class SingleColorPalette extends Component {
     }
     render() {
         const {format} = this.state;
-        const { paletteName, emoji } = this.props.palette;
+        const { paletteName, emoji, id } = this.props.palette;
         const colorBoxes = this._shades.map(color => (
-            <ColorBox key={color.id} name={color.name} background={color[format]} showLInk={false}/>
+            <ColorBox key={color.name} name={color.name} background={color[format]} showLInk={false}/>
         ));
         return (
-            <div className="Palette">
+            <div className="SingleColorPalette Palette">
                 <Navbar handleChange={this.changeFormat} showingAllColors={false}/> 
                 <div className = "Palette-colors">
                     {colorBoxes}
+                    <div className="go-back ColorBox">
+                        <Link to={`/palette/${id}`} className="back-button">GO BACK</Link>
+                        </div>
                 </div>
                 <PaletteFooter paletteName={paletteName} emoji={emoji}/>
             </div>
