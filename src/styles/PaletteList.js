691f3b3f846a97923560282e8574d49bef097a01
Zachary Deacon <zackdeacon347@gmail.com>
2021-01-27 15:59:46 -0800

    added responsiveness to palette list

--- src/styles/PaletteList.js
+++ src/styles/PaletteList.js
@@ -1,3 +1,5 @@
+import sizes from "./Sizes";
+
 export default {
     root: {
         height: "100vh",
@@ -12,7 +14,14 @@ export default {
         display: "flex",
         alignItems: "flex-start",
         flexDirection: "column",
-        flexWrap: "wrap"
+        flexWrap: "wrap",
+        [sizes.down("xl")]:{
+            width: "80%"
+        },
+        [sizes.down("xs")]:{
+            width: "75%"
+        }
+
     },
     nav: {
         display: "flex",
@@ -31,7 +40,15 @@ export default {
         width: "100%",
         display: "grid",
         gridTemplateColumns: "repeat(3, 30%)",
-        gridGap: "5%"
+        gridGap: "2.5rem",
+        [sizes.down("md")]:{
+        gridTemplateColumns: "repeat(2, 50%)",
+    
+        },    
+        [sizes.down("xs")]:{
+        gridTemplateColumns: "repeat(1, 100%)",
+        gridGap: "1rem"
+        }
 
     }
 };
